0932dd31799b16e4f683736a622d3cba760c7cba
palmer-jackson <jacksonpalmer@unr.edu>
2025-10-01 16:21:10 -0700

    New Player Sprite and Animations
    added idle and walking animations to new player sprite, still need to add more animations for jump, attack, etc.

--- Assets/Scripts/PlayerController2D.cs
+++ Assets/Scripts/PlayerController2D.cs
@@ -1,54 +1,4 @@
-// using UnityEngine;
 
-// public class PlayerController2D : MonoBehaviour
-// {
-//     [Header("Move")]
-//     public float moveSpeed = 6f;
-
-//     [Header("Jump")]
-//     public float jumpForce = 12f;
-//     public Transform groundCheck;          // drag your GroundCheck here
-//     public float groundCheckRadius = 0.15f;
-//     public LayerMask groundLayer;          // set to Ground layer in Inspector
-
-//     private Rigidbody2D rb;
-//     private bool isGrounded;
-
-//     void Awake()
-//     {
-//         rb = GetComponent<Rigidbody2D>();
-//     }
-
-//     void Update()
-//     {
-//         // Left/Right 
-     
-//         float x = Input.GetAxisRaw("Horizontal"); 
-//         rb.linearVelocity = new Vector2(x * moveSpeed, rb.linearVelocity.y);
-
-//         // Ground check 
-//         if (groundCheck != null)
-//         {
-//             isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
-//         }
-
-//         //  Jump on Space (only if grounded) 
-//         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-//         {
-//             // clear any downward speed before jump
-//             rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
-//             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-//         }
-//     }
-
-//     // gizmo check or something
-//     void OnDrawGizmosSelected()
-//     {
-//         if (groundCheck == null) return;
-//         Gizmos.color = Color.yellow;
-//         Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
-//     }
-// }
 using UnityEngine;
 
 public class PlayerController2D : MonoBehaviour
@@ -72,10 +22,12 @@ public class PlayerController2D : MonoBehaviour
 
     private Rigidbody2D rb;
     private bool isGrounded;
+    private Animator animator;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponentInChildren<Animator>(); 
     }
 
     void Update()
@@ -93,6 +45,8 @@ public class PlayerController2D : MonoBehaviour
         {
             transform.localScale = new Vector3(-1, 1, 1);
         }
+
+        animator.SetBool("isWalking", Mathf.Abs(x) > 0.01f);
 
         // Ground check
         isGrounded = Physics2D.OverlapBox(groundCheck.position, groundCheckSize, 0f, groundLayer);
