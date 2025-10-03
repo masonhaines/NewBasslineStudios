807eee1b18812c7ad1bcabca5a0f1638902199c0
Thomas-John <123978285+ThomasJVasile@users.noreply.github.com>
2025-10-02 22:00:30 -0700

    implemented level 2 and 3 buttons in the level select

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,10 +1,64 @@
+// using UnityEngine;
+// using UnityEngine.SceneManagement;
+
+// public class SceneLoader : MonoBehaviour
+// {
+//     public void LoadFirstLevel() { SceneManager.LoadScene(1); } // assumes Lv1 is build index 1
+//     public void QuitGame() {
+//         Application.Quit();
+//         #if UNITY_EDITOR
+//         UnityEditor.EditorApplication.isPlaying = false;
+//         #endif
+//     }
+// }
+
+
+
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneLoader : MonoBehaviour
 {
-    public void LoadFirstLevel() { SceneManager.LoadScene(1); } // assumes Lv1 is build index 1
-    public void QuitGame() {
+    // Optional: if you put this component on a Button and fill this field,
+    // it will auto-wire the click and gray out if the scene isn't in Build Settings.
+    [SerializeField] string sceneToLoad;
+
+    void Awake()
+    {
+        var btn = GetComponent<Button>();
+        if (btn && !string.IsNullOrEmpty(sceneToLoad))
+        {
+            btn.onClick.AddListener(() => LoadByName(sceneToLoad));
+            btn.interactable = Application.CanStreamedLevelBeLoaded(sceneToLoad);
+        }
+    }
+
+    // --- Simple no-arg handlers you can call from OnClick ---
+    public void LoadLevel1() => LoadByName("Level11");  // <-- match your actual Level 1 name
+    public void LoadLevel2() => LoadByName("Level2");
+    public void LoadLevel3() => LoadByName("Level3");
+
+    // Generic loaders
+    public void LoadByIndex(int buildIndex)
+    {
+        if (buildIndex >= 0 && buildIndex < SceneManager.sceneCountInBuildSettings)
+            SceneManager.LoadScene(buildIndex);
+        else
+            Debug.LogWarning($"Build index {buildIndex} isn't in Build Settings.");
+    }
+
+    public void LoadByName(string sceneName)
+    {
+        if (Application.CanStreamedLevelBeLoaded(sceneName))
+            SceneManager.LoadScene(sceneName);
+        else
+            Debug.LogWarning($"Scene '{sceneName}' is not in Build Settings or not saved.");
+    }
+
+    public void QuitGame()
+    {
         Application.Quit();
         #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
@@ -12,27 +66,3 @@ public class SceneLoader : MonoBehaviour
     }
 }
 
-// using UnityEngine;
-// using UnityEngine.SceneManagement;
-// using UnityEngine.UI;
-
-// [RequireComponent(typeof(Button))]
-// public class SceneLoader : MonoBehaviour
-// {
-//     [SerializeField] private string sceneName;
-
-//     public void Load()
-//     {
-//         if (Application.CanStreamedLevelBeLoaded(sceneName))
-//             SceneManager.LoadScene(sceneName);
-//         else
-//             Debug.LogWarning($"Scene '{sceneName}' is not in Build Settings (or not created yet).");
-//     }
-
-//     void Awake()
-//     {
-//         // Auto-disable if scene isn’t available yet
-//         var btn = GetComponent<Button>();
-//         if (btn) btn.interactable = Application.CanStreamedLevelBeLoaded(sceneName);
-//     }
-// }
