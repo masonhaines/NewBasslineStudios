9c8e2b581b0887c1bd44379a24edd6450b0181fb
masonhaines <levi.haines97@gmail.com>
2025-10-03 20:02:51 -0700

    created a hazard movement system
    currently trying to get rid of a bug where the enemy is damaged by other enemies

--- Assets/Scripts/AIStateSystem/Components/ChaseComponent.cs
+++ Assets/Scripts/AIStateSystem/Components/ChaseComponent.cs
@@ -3,11 +3,6 @@ using UnityEngine;
 
 public class ChaseComponent : MonoBehaviour
 {
-    // private List<Transform> waypoints;
-    // private int numberOfWaypointPositions;
-    // private Vector2 targetPosition;
-
-
     private ITarget movementController;
 
     private void Awake()
@@ -15,7 +10,6 @@ public class ChaseComponent : MonoBehaviour
         // moveRef = GetComponentInParent<ITarget>(); // reference to all other objects that have implement interface in parent prefab
         // moveRef = GetComponent<ITarget>() ?? GetComponentInParent<ITarget>();
         movementController = GetComponent<ITarget>(); // reference to all other objects that have implement interface in parent prefab
-        // waypoints = new List<Transform>();
     }
     private void Start()
     {
@@ -34,22 +28,6 @@ public class ChaseComponent : MonoBehaviour
         if (!target) return;
         movementController.NewTargetLocation(target.position);
     }
-
-    // public void OnTargetReachedListener()
-    // {
-    //     if(movementController == null) return;
-    //     targetPosition = waypoints[0].position;
-    //     movementController.NewTargetLocation(targetPosition);
-    //     waypoints.RemoveAt(0);
-    // }
-    //
-    // // call me in the update of ai controller
-    // public void GetNewWaypoint(Transform targetWaypoint)
-    // {
-    //     if (!targetWaypoint) return;
-    //     waypoints.Add(targetWaypoint);
-    //     
-    // }
 }
 
 
---
+++ Assets/Scripts/AIStateSystem/Components/HazardMovement.cs
@@ -0,0 +1,97 @@
+using UnityEngine;
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine.UIElements.Experimental;
+public class HazardMovement : MonoBehaviour
+{
+     
+    
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private int newGravityScale;
+    [SerializeField] private float yDistanceToMove = 100f;
+    [SerializeField] private float xDistanceToMove = 100f;
+    [SerializeField] private float radius = 5f;
+    [SerializeField] private float waveAmplitude = 1.5f;
+    [SerializeField] private bool moveStraight;
+    [SerializeField] private float distanceBeforeFall;
+
+    
+    private Rigidbody2D moversRigidbody2D;
+    private Vector2 targetLocation;
+    private Vector2 movementVector;
+    private Vector2 activeTargetLocation;
+    private bool isFalling;
+    
+    
+    private void Awake()
+    {
+        moversRigidbody2D = GetComponent<Rigidbody2D>();
+        movementVector = new Vector2(xDistanceToMove, yDistanceToMove);
+        targetLocation = moversRigidbody2D.position + movementVector;
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        Moving();
+    }
+
+    public void Moving()
+    {
+        Debug.DrawLine(targetLocation, moversRigidbody2D.position, Color.red);
+        
+        if (moveStraight)
+        {
+            var distanceTravelled = Vector2.Distance(moversRigidbody2D.position, targetLocation);
+            if ( distanceTravelled < 1.5f)
+            {
+                movementVector = -movementVector;
+                targetLocation = moversRigidbody2D.position + movementVector;
+            }
+
+            float offsetY = Mathf.Sin(Time.time * moveSpeed) * waveAmplitude;
+            activeTargetLocation = targetLocation + new Vector2(0, offsetY);
+        }
+        else
+        {
+            var xCos = Mathf.Cos(Time.time * moveSpeed) * radius;
+            var ySin = Mathf.Sin(Time.time * moveSpeed) * radius;
+            var waveVector = new Vector2(xCos * xDistanceToMove, ySin * yDistanceToMove);
+            
+            targetLocation = moversRigidbody2D.position + movementVector + waveVector;
+            activeTargetLocation = targetLocation;
+        }
+        
+        Vector2 moveTowardsPosition = Vector2.MoveTowards
+        (
+            moversRigidbody2D.position, 
+            activeTargetLocation, 
+            moveSpeed * Time.deltaTime
+        );
+        
+        Vector2 direction = targetLocation - moversRigidbody2D.position;
+        
+        switch (direction.x)
+        {
+            case > 0.1f:
+                // SpriteRenderer.flipX = true; // only flips sprite
+                transform.localScale = new Vector3(1, 1, 1);  // face left
+                break;
+            case < -0.1f:
+                // SpriteRenderer.flipX = false; // only flips sprite
+                transform.localScale = new Vector3(-1, 1, 1);  // face left
+                break;
+        }
+        
+        // where movement is actually happening 
+        moversRigidbody2D.MovePosition(moveTowardsPosition);
+    }
+    
+    // private IEnumerator FallingCoroutine()
+    // {
+    //     moversRigidbody2D.gravityScale = newGravityScale;
+    //     yield return new WaitForSeconds(.5f);
+    //     moversRigidbody2D.gravityScale = 0f;
+    //
+    // }
+}
