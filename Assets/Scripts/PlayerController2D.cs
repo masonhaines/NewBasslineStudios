a9fe18ffa52333c12943a9882d8908dc5b223d49
palmer-jackson <jacksonpalmer@unr.edu>
2025-12-01 21:44:46 -0800

    added dash
    tried also adding trail but cant get it to be visible

--- Assets/Scripts/PlayerController2D.cs
+++ Assets/Scripts/PlayerController2D.cs
@@ -1,194 +1,3 @@
-// using UnityEngine;
-
-// public class PlayerController2D : MonoBehaviour
-// {
-    
-    
-//     [Header("Movement")]
-//     public float moveSpeed = 6f;
-
-//     [Header("Jumping")]
-//     public float jumpForce = 12f;
-//     public Transform groundCheck;
-//     public Vector2 groundCheckSize = new Vector2(0.6f, 0.1f);
-//     public LayerMask groundLayer;
-//     public int extraJumps = 1;
-//     private int jumpsLeft;
-
-//     [Header("Coyote Time & Jump Buffer")]
-//     public float coyoteTime = 0.15f;
-//     private float coyoteTimeCounter;
-//     public float jumpBufferTime = 0.15f;
-//     private float jumpBufferCounter;
-
-//     [Header("Attack")]
-//     public float attackCooldown = 0.3f; // delay before next attack
-//     private float attackTimer;
-//     private bool isAttacking;
-//     [SerializeField] private Collider2D weapon; // this needs to be given a collider box inside of the editor, from mason 
-
-//     private Rigidbody2D rb;
-//     private HealthComponent healthComponentObject;
-//     private Animator animator;
-
-//     private bool isGrounded;
-
-//     void Awake()
-//     {
-//         rb = GetComponent<Rigidbody2D>();
-//         animator = GetComponentInChildren<Animator>();
-//         healthComponentObject = GetComponent<HealthComponent>();
-//         DisableWeaponHitbox();
-//     }
-
-//     void Update()
-//     {
-//         if (!healthComponentObject.GetIsKnockedBack())
-//         {
-//             HandleAttack();
-
-//             // Disable horizontal movement while attacking (on ground)
-//             if (!isAttacking)
-//                 HandleMovement();
-//         }
-
-//         HandleJumping();
-//     }
-
-    
-//     void HandleMovement()
-//     {
-//         float x = Input.GetAxisRaw("Horizontal");
-//         rb.linearVelocity = new Vector2(x * moveSpeed, rb.linearVelocity.y);
-
-//         // Flip sprite
-//         if (x > 0)
-//             transform.localScale = new Vector3(1, 1, 1);
-//         else if (x < 0)
-//             transform.localScale = new Vector3(-1, 1, 1);
-
-//         animator.SetBool("isWalking", Mathf.Abs(x) > 0.01f);
-//     }
-
-    
-//     void HandleJumping()
-//     {
-//         // Ground check
-//         isGrounded = Physics2D.OverlapBox(groundCheck.position, groundCheckSize, 0f, groundLayer);
-
-//         // Update jump animation
-//         animator.SetBool("isJumping", !isGrounded && rb.linearVelocity.y != 0);
-
-//         if (isGrounded)
-//             jumpsLeft = extraJumps;
-
-//         // Coyote & jump buffer timers
-//         coyoteTimeCounter = isGrounded ? coyoteTime : coyoteTimeCounter - Time.deltaTime;
-//         jumpBufferCounter -= Time.deltaTime;
-
-//         if (Input.GetKeyDown(KeyCode.Space))
-//             jumpBufferCounter = jumpBufferTime;
-
-//         if (jumpBufferCounter > 0f)
-//         {
-//             if (coyoteTimeCounter > 0f)
-//             {
-//                 DoJump();
-//                 jumpBufferCounter = 0f;
-//             }
-//             else if (jumpsLeft > 0)
-//             {
-//                 DoJump();
-//                 jumpsLeft--;
-//                 jumpBufferCounter = 0f;
-//             }
-//         }
-//     }
-
-//     void DoJump()
-//     {
-//         rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
-//         rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-//         coyoteTimeCounter = 0f;
-//     }
-
-    
-//     void HandleAttack()
-//     {
-//         // Decrease cooldown timer
-//         attackTimer -= Time.deltaTime;
-
-//         // Only allow new attack when cooldown is ready and not currently attacking
-//         if (Input.GetMouseButtonDown(0) && attackTimer <= 0f && !isAttacking)
-//         {
-//             animator.SetTrigger("attack");
-//             attackTimer = attackCooldown;
-//         }
-//     }
-
-//     // Called from animation event at the start of attack animation
-//     public void StartAttack()
-//     {
-//         isAttacking = true;
-//         EnableWeaponHitbox();
-
-//         // Stop horizontal movement during attack (only on ground)
-//         if (isGrounded)
-//             rb.linearVelocity = new Vector2(0f, rb.linearVelocity.y);
-//     }
-
-//     // Called from animation event at the end of attack animation
-//     public void EndAttack()
-//     {
-//         isAttacking = false;
-//         DisableWeaponHitbox();
-//     }
-
-//     void EnableWeaponHitbox()
-//     {
-//         // Transform weapon = transform.Find("Weapon");
-//         if (weapon != null)
-//         {
-//             // var col = weapon.GetComponent<BoxCollider2D>();
-//             if (weapon != null)
-//                 weapon.enabled = true;
-//             else 
-//                 Debug.Log("was not able to find weapon hit box");
-//         }
-//         else
-//         {
-//             Debug.Log("was not able to find weapon hit box");
-//         }
-
-//     }
-
-//     void DisableWeaponHitbox()
-//     {
-//         // Transform weapon = transform.Find("Weapon");
-//         if (weapon != null)
-//         {
-//             // var col = weapon.GetComponent<BoxCollider2D>();
-            
-//             if (weapon != null)
-//                 weapon.enabled = false;
-            
-//             else 
-//                 Debug.Log("was not able to find weapon hit box");
-//         }
-//         else
-//         {
-//             Debug.Log("was not able to find weapon hit box");
-//         }
-//     }
-
-
-//     void OnDrawGizmosSelected()
-//     {
-//         if (groundCheck == null) return;
-//         Gizmos.color = Color.yellow;
-//         Gizmos.DrawWireCube(groundCheck.position, groundCheckSize);
-//     }
-// }
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
@@ -196,8 +5,17 @@ public class PlayerController2D : MonoBehaviour
 {
     [Header("Movement")]
     public float moveSpeed = 6f;
-
     public float InitMoveSpeed;
+
+    [Header("Dash Settings")]
+    public float dashSpeed = 20f;
+    public float dashDuration = 0.18f;
+    public float dashCooldown = 0.6f;
+    private float dashTimer = 0f;
+    private float dashCooldownTimer = 0f;
+    private bool isDashing = false;
+
+    public TrailRenderer trail;
 
     [Header("Jumping")]
     public float jumpForce = 12f;
@@ -215,15 +33,15 @@ public class PlayerController2D : MonoBehaviour
 
     [Header("Melee Attack")]
     public float attackCooldown = 0.3f;
-    private float attackTimer;
-    private bool isAttacking;
+    private float attackTimer = 0f;
+    private bool isAttacking = false;
     [SerializeField] private Collider2D weapon;
 
     [Header("Projectile Attack")]
     public GameObject projectilePrefab;
     public Transform firePoint;
     public float projectileCooldown = 0.5f;
-    private float projectileTimer;
+    private float projectileTimer = 0f;
 
     private Rigidbody2D rb;
     private HealthComponent healthComponentObject;
@@ -238,27 +56,78 @@ public class PlayerController2D : MonoBehaviour
         animator = GetComponentInChildren<Animator>();
         healthComponentObject = GetComponent<HealthComponent>();
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
-        InitColor = spriteRenderer.color;
+        InitColor = spriteRenderer != null ? spriteRenderer.color : Color.white;
         DisableWeaponHitbox();
         InitMoveSpeed = moveSpeed;
+
+        
+        if (trail == null)
+            trail = GetComponentInChildren<TrailRenderer>();
+        if (trail != null)
+            trail.emitting = false;
+
+        
+        dashCooldownTimer = 0f;
+        dashTimer = 0f;
+        projectileTimer = 0f;
+        attackTimer = 0f;
     }
 
     void Update()
     {
-        // Debug.Log(healthComponentObject.currentHealth);
         if (!healthComponentObject.GetIsKnockedBack())
         {
+            HandleDash();
             HandleAttack();
             HandleProjectileAttack();
 
-            if (!isAttacking)
+            
+            if (!isAttacking && !isDashing)
                 HandleMovement();
         }
 
         HandleJumping();
     }
 
-    // -------------------- Movement --------------------
+    void HandleDash()
+    {
+        
+        dashCooldownTimer -= Time.deltaTime;
+
+        // leftshift to dash
+        if (!isDashing && Input.GetKeyDown(KeyCode.LeftShift) && dashCooldownTimer <= 0f && !isAttacking)
+        {
+            isDashing = true;
+            dashTimer = dashDuration;
+            dashCooldownTimer = dashCooldown;
+
+            
+            if (trail != null)
+                trail.emitting = true;
+        }
+
+        if (isDashing)
+        {
+            float dir = transform.localScale.x >= 0 ? 1f : -1f;
+
+            //set horizontal velocity to dashSpeed
+            rb.linearVelocity = new Vector2(dir * dashSpeed, rb.linearVelocity.y);
+
+            dashTimer -= Time.deltaTime;
+            if (dashTimer <= 0f)
+            {
+                isDashing = false;
+
+                
+                if (trail != null)
+                    trail.emitting = false;
+
+                // restore moveSpeed
+                moveSpeed = InitMoveSpeed;
+            }
+        }
+    }
+
     void HandleMovement()
     {
         float x = Input.GetAxisRaw("Horizontal");
@@ -270,22 +139,23 @@ public class PlayerController2D : MonoBehaviour
         else if (x < 0)
             transform.localScale = new Vector3(-1, 1, 1);
 
-        animator.SetBool("isWalking", Mathf.Abs(x) > 0.01f);
+        if (animator != null)
+            animator.SetBool("isWalking", Mathf.Abs(x) > 0.01f);
     }
 
-    // -------------------- Jumping --------------------
     void HandleJumping()
     {
-        isGrounded = Physics2D.OverlapBox(groundCheck.position, groundCheckSize, 0f, groundLayer);
-        // animator.SetBool("isJumping", !isGrounded && rb.linearVelocity.y != 0);
+        if (groundCheck == null)
+            return;
 
-        // if (isGrounded)
-        //     jumpsLeft = extraJumps;
-        
+        isGrounded = Physics2D.OverlapBox(groundCheck.position, groundCheckSize, 0f, groundLayer);
+
+        // preserve previous behavior: reset extra jumps and update animator
         if (isGrounded)
         {
             jumpsLeft = extraJumps;
-            animator.SetBool("isJumping", false);
+            if (animator != null)
+                animator.SetBool("isJumping", false);
         }
 
         coyoteTimeCounter = isGrounded ? coyoteTime : coyoteTimeCounter - Time.deltaTime;
@@ -315,18 +185,19 @@ public class PlayerController2D : MonoBehaviour
         rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
         rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         coyoteTimeCounter = 0f;
-        
-        animator.SetBool("isJumping", true);
+
+        if (animator != null)
+            animator.SetBool("isJumping", true);
     }
 
-    // -------------------- Melee Attack --------------------
     void HandleAttack()
     {
         attackTimer -= Time.deltaTime;
 
-        if (Input.GetMouseButtonDown(0) && attackTimer <= 0f && !isAttacking)
+        if (Input.GetMouseButtonDown(0) && attackTimer <= 0f && !isAttacking && !isDashing)
         {
-            animator.SetTrigger("attack");
+            if (animator != null)
+                animator.SetTrigger("attack");
             attackTimer = attackCooldown;
         }
     }
@@ -346,50 +217,40 @@ public class PlayerController2D : MonoBehaviour
         DisableWeaponHitbox();
     }
 
-    // -------------------- Projectile Attack --------------------
     void HandleProjectileAttack()
-{
-    projectileTimer -= Time.deltaTime;
-
-    if (Input.GetMouseButtonDown(1) && projectileTimer <= 0f)
     {
-        animator.SetTrigger("rangedAttack"); // optional animation
-        //FireProjectile();
-        projectileTimer = projectileCooldown;
-    }
-}
+        projectileTimer -= Time.deltaTime;
 
-public void FireProjectile()
-{
-    if (projectilePrefab == null || firePoint == null)
-    {
-        Debug.LogWarning("ProjectilePrefab or FirePoint not assigned!");
-        return;
+        if (Input.GetMouseButtonDown(1) && projectileTimer <= 0f)
+        {
+            if (animator != null)
+                animator.SetTrigger("rangedAttack"); 
+            projectileTimer = projectileCooldown;
+        }
     }
 
-    // Instantiate projectile
-    GameObject proj = Instantiate(projectilePrefab, firePoint.position, Quaternion.identity);
-
-    // Determine shooting direction based on facing
-    Vector2 shootDir = transform.localScale.x > 0 ? Vector2.right : Vector2.left;
-
-    // Flip the projectile sprite if facing left
-    if (transform.localScale.x < 0)
-        proj.transform.localScale = new Vector3(-1, 1, 1);
-
-    // Get the PlayerProjectile script and initialize it properly
-    PlayerProjectile projectile = proj.GetComponent<PlayerProjectile>();
-    if (projectile != null)
+    public void FireProjectile()
     {
-        projectile.Initialize(shootDir, gameObject, rb.linearVelocity.x);
-    }
-    else
-    {
-        Debug.LogError("Spawned projectile is missing the PlayerProjectile component!");
-    }
-}
+        if (projectilePrefab == null || firePoint == null) return;
 
-    // -------------------- Weapon Collider Helpers --------------------
+        GameObject proj = Instantiate(projectilePrefab, firePoint.position, Quaternion.identity);
+
+        Vector2 shootDir = transform.localScale.x > 0 ? Vector2.right : Vector2.left;
+
+        // Flip projectile visually if facing left
+        if (transform.localScale.x < 0)
+            proj.transform.localScale = new Vector3(-1, 1, 1);
+
+        PlayerProjectile projectile = proj.GetComponent<PlayerProjectile>();
+        if (projectile != null)
+            projectile.Initialize(shootDir, gameObject, rb.linearVelocity.x);
+    }
+
+    public void SpawnProjectile()
+    {
+        FireProjectile();
+    }
+
     void EnableWeaponHitbox()
     {
         if (weapon != null)
@@ -402,11 +263,8 @@ public void FireProjectile()
     {
         if (weapon != null)
             weapon.enabled = false;
-        else
-            Debug.Log("Weapon hitbox not assigned!");
     }
 
-    // -------------------- Debug --------------------
     void OnDrawGizmosSelected()
     {
         if (groundCheck == null) return;
